141cbd22510ee26d8f2af83d056eee754d0a6575
= <=>
2026-03-21 01:12:46 -0300

    feat: Iniciando desenvolvimento da API, com a criação das pastas essenciais e do CorsFilter

---
+++ H3althierLiving(API)/src/main/java/br/com/fiap/H3althierLiving/H3althierLivingApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.H3althierLiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H3althierLivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H3althierLivingApplication.class, args);
+	}
+
+}
---
+++ H3althierLiving(API)/src/test/java/br/com/fiap/H3althierLiving/H3althierLivingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.H3althierLiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H3althierLivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
